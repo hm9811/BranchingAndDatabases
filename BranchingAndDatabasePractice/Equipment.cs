17ee647fe1347978af84a81be67826da44799a49
hm9811 <hm981198@gmail.com>
2021-01-11 14:12:31 -0800

    Created a class for computer equipment

---
+++ BranchingAndDatabasePractice/Equipment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAndDatabasePractice
+{
+    /// <summary>
+    /// Represents a single piece of computer equipment
+    /// </summary>
+    class Equipment
+    {
+        /// <summary>
+        /// The primary key of the equipment
+        /// </summary>
+        public int EquipmentId { get; set; }
+
+        /// <summary>
+        /// The name of the equipment
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The retail price of the equipment
+        /// </summary>
+        public double Price { get; set; }
+    }
+}
